f6a50c832f826f8b383219e0562183afd9ec7dd2
Peter Vivo <vivo.peter@protonmail.comgit commit -m retrogit pushgit commit -m next>
2025-01-01 20:14:30 +0100

    put, get, delete

--- src/services/dbServer.js
+++ src/services/dbServer.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 import cors from 'cors';
 import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT;
-const TABLE_NAME = process.env.DYNAMO_TABLE;
+const TableName = process.env.DYNAMO_TABLE;
 
 app.use(cors({
   origin: 'http://localhost:5173',
@@ -17,33 +17,49 @@ app.use(cors({
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-const db = new DynamoDBClient({
+const dynamo = new DynamoDBClient({
   region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   }
 });
+const db = DynamoDBDocument.from(dynamo);
 
 // Health check endpoint
 app.get('/health', (req, res) => {
-  res.json({ status: TABLE_NAME || '-- fine --', timestamp: new Date().toISOString() });
+  res.json({ status: TableName, timestamp: new Date().toISOString() });
 });
 
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html
+
 /** @type {(req:Request,res:Response) => Response} */
-app.post('/add', (req, res) => {
-  console.log(req.body);
-  const {id, content} = req.body;
-  const command = new PutItemCommand({
-    TableName: TABLE_NAME,
-    Item: {
-      id: { S: id },
-      content: { S: content }
-    } 
-  });
-  db.send(command).then(console.log)
-  res.json({data: req.body});
-})
+app.post('/put', (req, res) => {
+  db.put({
+    TableName,
+    Item: req.body,
+  })
+    .then(r => res.json(r))
+    .catch(console.warn);
+});
+
+app.post('/get', (req, res) => {
+  db.get({
+    TableName,
+    Key: {...req.body}
+  })
+    .then(({Item}) => res.json(Item))
+    .catch(console.warn);
+});
+
+app.post('/delete', (req, res) => {
+  db.delete({
+    TableName,
+    Key: {...req.body}
+  })
+    .then(r => res.json(r))
+    .catch(console.warn);
+});
 
 // Start server
 app.listen(PORT, () => {
