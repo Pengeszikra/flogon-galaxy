8673494a308532e6ef6584568e12f21a045a06f3
Peter Vivo <vivo.peter@protonmail.comgit commit -m retrogit pushgit commit -m next>
2025-01-05 21:41:25 +0100

    before fencer have style

--- src/quest.jsx
+++ src/quest.jsx
@@ -7,10 +7,16 @@ portal(
       front={"../sheets/texture-2002.png"}
       back={"../sheets/texture-2001.png"}
     />
-    <section class="grid place-items-center absolute top-0 left-0 min-w-[100vw] min-h-[100%] sele">
-      <section class="grid">
-      <h1 class="text-emerald-200 text-2xl select-none">F L O G O N - G A L A X Y</h1>
-      <p class="text-orange-300 text-sm text-right min-w-max">first contact</p>
+    <section class="
+      grid place-items-center
+      absolute top-0 left-0
+      min-w-[100vw] min-h-[100%]
+      selection-none
+      --bg-[url(../sheets/texture-2012.png)]
+    ">
+      <section class="grid select-none">
+        <h1 class="text-emerald-200 text-2xl select-none">F L O G O N - G A L A X Y</h1>
+        <p class="text-orange-300 text-sm text-right min-w-max">first contact</p>
       </section>
     </section>
   </section>
@@ -18,3 +24,22 @@ portal(
 
 const [state] = routeController();
 state.ySpeed = .3;
+
+document.addEventListener("keydown",
+  /** @type {(event:KeyboardEvent) => any} */
+  (event) => {
+    const {key} = event;
+    switch (key) {
+      case "a": return state.xSpeed -= .1;
+      case "d": return state.xSpeed += .1;
+      case "w": return state.ySpeed -= .1;
+      case "s": return state.ySpeed += .1;
+      case " ": {
+        state.xSpeed = 0;
+        state.ySpeed = 0;
+        state.zSpeed = 0;
+        return;
+      };
+    }
+  }
+);
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,4 +9,9 @@ export default {
     extend: {},
   },
   plugins: [],
+  safelist: [
+    {
+      pattern: /bg-\[url\(.*?\)\]/, // Whitelist dynamic bg-[url(...)] classes
+    }
+  ],
 }
