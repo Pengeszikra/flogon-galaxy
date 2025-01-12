09420cecb60760e70d186c905f15ac3b6fd505fe
Peter Vivo <vivo.peter@protonmail.comgit commit -m retrogit pushgit commit -m next>
2025-01-12 01:41:04 +0100

    start test

--- src/quest.jsx
+++ src/quest.jsx
@@ -4,17 +4,20 @@ import { GalaxyRoute, galaxyTextureList, routeController, useKeyboardCurse } fro
 const [gAlfa, gBeta] = galaxyTextureList();
 
 portal(
-  <GalaxyRoute
-    front={`../sheets/texture-${gAlfa}.png`}
-    back={`public/ui-elements/fare-and-beyond-statue.png`}
-    zoom={250}
-  >
-    <section class="grid select-none">
-      <h1 class="text-emerald-200 text-[5vw]">F L O G O N - G A L A X Y</h1>
-      <p class="text-orange-300 text-[3vw] text-right min-w-max">first contact</p>
-    </section>
-  </GalaxyRoute>
-).then(() => {
+  <section>
+    <GalaxyRoute
+      front={`../sheets/texture-${gAlfa}.png`}
+      back={`public/ui-elements/fare-and-beyond-statue.png`}
+      zoom={250}
+    >
+      <section class="grid select-none">
+        <h1 class="text-emerald-200 text-[5vw]">F L O G O N - G A L A X Y</h1>
+        <p class="text-orange-300 text-[3vw] text-right min-w-max">first contact</p>
+      </section>
+    </GalaxyRoute>
+  </section>
+).then((page) => {
+  page.onclick = () => globalThis.location.replace('deal.html');
   const [state] = routeController("multiply");
   state.ySpeed = Math.random() - .5;
   state.xSpeed = Math.random() - .5;
